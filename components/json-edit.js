44b3e43dc855760ac273fd6cab79f44b316dc118
Cerem Cem ASLAN <cem@aktos.io>
2018-03-09 15:57:15 +0300

    improved fixJSON function

--- components/json-edit.js
+++ components/json-edit.js
@@ -1,3 +1,25 @@
+function fixJSON(badJSON){
+    //taken from https://stackoverflow.com/a/39050609/1952991 (Malvineous)
+    return badJSON
+
+    // Replace ":" with "@colon@" if it's between double-quotes
+    .replace(/:\s*"([^"]*)"/g, function(match, p1) {
+        return ': "' + p1.replace(/:/g, '@colon@') + '"';
+    })
+
+    // Replace ":" with "@colon@" if it's between single-quotes
+    .replace(/:\s*'([^']*)'/g, function(match, p1) {
+        return ': "' + p1.replace(/:/g, '@colon@') + '"';
+    })
+
+    // Add double-quotes around any tokens before the remaining ":"
+    .replace(/(['"])?([a-z0-9A-Z_]+)(['"])?\s*:/g, '"$2": ')
+
+    // Turn "@colon@" back into ":"
+    .replace(/@colon@/g, ':')
+;
+}
+
 Ractive.components['json-edit'] = Ractive.extend({
   template: `<textarea style="white-space: pre-wrap">{{ objFormatted }}</textarea>`,
   isolated: true,
@@ -12,10 +34,9 @@ Ractive.components['json-edit'] = Ractive.extend({
         }
       },
       set: function(objStr){
-        var obj, e, fixedJSON;
+        var obj, e;
         try {
-            fixedJSON = objStr.replace(/(['"])?([a-zA-Z0-9_]+)(['"])?:/g, '"$2": ');
-          obj = JSON.parse(fixedJSON);
+          obj = JSON.parse(fixJSON(objStr));
           this.set('value', obj);
           this.set('objTmp', null);
           return
@@ -27,3 +48,20 @@ Ractive.components['json-edit'] = Ractive.extend({
     }
   }
 });
+/*
+new Ractive({
+	el: 'body',
+	template: `
+		<h2>Input</h2>
+		<p>Type a JSON here: </p>
+		<json-edit value="{{foo}}" /> 
+		<h2>Output:</h2>
+		<pre>{{JSON.stringify(foo)}}</pre>
+	`,
+	onrender: function(){
+		this.observe('foo', function(value){
+			console.log("foo is changed", value)
+		})
+	}	
+})
+*/
